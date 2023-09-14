bd28dc1c6e5e3429173af7fc2bf02950f5673c36
jesus <a20196324@pucp.edu.pe>
2023-09-13 23:56:22 -0500

    init

---
+++ app/src/main/java/com/example/telecommunity/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.telecommunity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button loginButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
