6f70eb21245ec93b4c19080480d123a4055fc2c6
jesus <a20196324@pucp.edu.pe>
2023-09-14 00:31:45 -0500

    vistas logueo

--- app/src/main/java/com/example/telecommunity/IniciarSesion.java
+++ app/src/main/java/com/example/telecommunity/IniciarSesion.java
@@ -2,7 +2,11 @@ package com.example.telecommunity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class IniciarSesion extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class IniciarSesion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
+
+
+        //redireccionar a registro
+        TextView registrarme = findViewById(R.id.signupText);
+        registrarme.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent intent = new Intent(IniciarSesion.this, RegistroUsuario.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/telecommunity/RegistroUsuario.java
+++ app/src/main/java/com/example/telecommunity/RegistroUsuario.java
@@ -2,7 +2,10 @@ package com.example.telecommunity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegistroUsuario extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class RegistroUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
+
+        //redireccionar a logueo
+        TextView iniciarme = findViewById(R.id.loginRedirectText);
+        iniciarme.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent intent = new Intent(RegistroUsuario.this, IniciarSesion.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
