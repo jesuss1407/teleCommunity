c7be9d353dd7b3d0cd51a7b8a8a74f08ce995776
jesus <a20196324@pucp.edu.pe>
2023-10-19 17:34:29 -0500

    registro funka

--- app/src/main/java/com/example/telecommunity/IniciarSesion.java
+++ app/src/main/java/com/example/telecommunity/IniciarSesion.java
@@ -6,25 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+
+import com.example.telecommunity.databinding.ActivityIniciarSesionBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 
 public class IniciarSesion extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private EditText emailEditText, passwordEditText;
+    private Button loginButton;
+    ActivityIniciarSesionBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
 
 
-        //iniciar
-        Button iniciar = findViewById(R.id.loginButton);
-        iniciar.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View view) {
-                Intent intent = new Intent(IniciarSesion.this, PantallaPrincipal.class);
-                startActivity(intent);
-            }
-        });
 
         //redireccionar a registro
         TextView registrarme = findViewById(R.id.signupText);
@@ -46,5 +51,41 @@ public class IniciarSesion extends AppCompatActivity {
             }
         });
 
+
+
+        //iniciar sesion
+
+        mAuth = FirebaseAuth.getInstance();
+        emailEditText = findViewById(R.id.correo);
+        passwordEditText = findViewById(R.id.contrasena);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (!email.isEmpty() && !password.isEmpty()) {
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(IniciarSesion.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Inicio de sesión exitoso
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        Toast.makeText(IniciarSesion.this, "Inicio de sesión exitoso.", Toast.LENGTH_SHORT).show();
+                                        // Redirigir a la actividad principal
+                                        startActivity(new Intent(IniciarSesion.this, MainActivity.class));
+                                    } else {
+                                        Toast.makeText(IniciarSesion.this, "Error en el inicio de sesión.", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                } else {
+                    Toast.makeText(IniciarSesion.this, "Por favor, complete todos los campos.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/telecommunity/RegistroUsuario.java
+++ app/src/main/java/com/example/telecommunity/RegistroUsuario.java
@@ -7,12 +7,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class RegistroUsuario extends AppCompatActivity {
+import android.util.Log;
+import android.widget.Toast;
 
+import com.example.telecommunity.databinding.ActivityRegistroUsuarioBinding;
+import com.example.telecommunity.entity.UsuariosDto;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.DocumentSnapshot;
+public class RegistroUsuario extends AppCompatActivity {
+    FirebaseFirestore db;
+    ActivityRegistroUsuarioBinding binding;
+    ListenerRegistration snapshotListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
+
+        binding = ActivityRegistroUsuarioBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        db = FirebaseFirestore.getInstance();
 
         //redireccionar a logueo
         TextView iniciarme = findViewById(R.id.loginRedirectText);
@@ -22,6 +39,50 @@ public class RegistroUsuario extends AppCompatActivity {
                 Intent intent = new Intent(RegistroUsuario.this, IniciarSesion.class);
                 startActivity(intent);
             }
+        });
+
+        //registro
+        binding.crearBtn.setOnClickListener(view -> {
+            String nombre = binding.nombre.getText().toString();
+            String apellido = binding.apellido.getText().toString();
+            String codigoStr = binding.codigo.getText().toString();
+            String correo = binding.correo.getText().toString();
+            String contrasena = binding.contrasena.getText().toString();
+            String contrasena2 = binding.contrasena2.getText().toString();
+            String condicion = binding.condicion.getText().toString();
+            String rol = "Usuario";
+            String estado = "1";
+            String foto = "link";
+
+            if (contrasena.equals(contrasena2)) {
+                UsuariosDto usuario = new UsuariosDto();
+                usuario.setNombre(nombre);
+                usuario.setApellido(apellido);
+                usuario.setCodigo(Integer.parseInt(codigoStr));
+                usuario.setCorreo(correo);
+                usuario.setContrasena(contrasena);
+                usuario.setCondicion(condicion);
+                usuario.setRol(rol);
+                usuario.setEstado(Integer.parseInt(estado));
+                usuario.setFoto(foto);
+
+
+                db.collection("usuarios")
+                        .document(codigoStr)
+                        .set(usuario)
+                        .addOnSuccessListener(unused -> {
+                            Toast.makeText(this, "Usuario registrado correctamente", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(RegistroUsuario.this, IniciarSesion.class);
+                            startActivity(intent);
+                        })
+                        .addOnFailureListener(e -> {
+                            Toast.makeText(this, "Ocurrió un error, intente nuevamente ", Toast.LENGTH_SHORT).show();
+                        });
+            } else {
+                System.out.println("Las palabras son diferentes.");
+            }
+
+
         });
 
 
