a4fd7ed32914561d2bb9e6df8f9e5d1987dd6be9
Marcelo Rojas PC <mrojasl@pucp.edu.pe>
2023-10-20 16:21:21 -0500

    Al presionar inicio, se cambia a Mis actividades

--- app/src/main/java/com/example/telecommunity/BaseActivity.java
+++ app/src/main/java/com/example/telecommunity/BaseActivity.java
@@ -6,17 +6,40 @@ import androidx.fragment.app.Fragment;
 import com.example.telecommunity.R;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BaseActivity extends AppCompatActivity {
+    private TextView titleTextView;
+    BottomNavigationView navView;
+    private boolean enMisActividades = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
 
-        BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
+        titleTextView = findViewById(R.id.titleTextView);
+        navView = findViewById(R.id.bottomNavigationView);
+
+        titleTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (enMisActividades) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                    titleTextView.setText("Inicio");
+                    enMisActividades = false;
+                } else {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MisActividadesFragment()).commit();
+                    titleTextView.setText("Mis Actividades");
+                    enMisActividades = true;
+                }
+            }
+        });
+
         navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -24,15 +47,25 @@ public class BaseActivity extends AppCompatActivity {
 
                 int id = item.getItemId();
                 if (id == R.id.navigation_inicio) {
-                    selectedFragment = new HomeFragment();
+                    if (enMisActividades) {
+                        selectedFragment = new MisActividadesFragment();
+                        titleTextView.setText("Mis Actividades");
+                    } else {
+                        selectedFragment = new HomeFragment();
+                        titleTextView.setText("Inicio");
+                    }
                 } else if (id == R.id.navigation_buscar) {
                     selectedFragment = new SearchFragment();
+                    titleTextView.setText("Buscar");
                 } else if (id == R.id.navigation_notificacion) {
                     selectedFragment = new NotificationFragment();
+                    titleTextView.setText("Notificaciones");
                 } else if (id == R.id.navigation_donacion) {
                     selectedFragment = new DonationFragment();
+                    titleTextView.setText("Donaciones");
                 } else if (id == R.id.navigation_mi_perfil) {
                     selectedFragment = new ProfileFragment();
+                    titleTextView.setText("Mi Perfil");
                 }
 
                 if (selectedFragment != null) {
@@ -43,10 +76,11 @@ public class BaseActivity extends AppCompatActivity {
             }
         });
 
-                if (savedInstanceState == null) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-                    navView.setSelectedItemId(R.id.navigation_inicio);
-                }
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            navView.setSelectedItemId(R.id.navigation_inicio);
+            titleTextView.setText("Inicio");
+        }
 
 
 
---
+++ app/src/main/java/com/example/telecommunity/MisActividadesFragment.java
@@ -0,0 +1,21 @@
+package com.example.telecommunity;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MisActividadesFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mis_actividades, container, false);
+    }
+}
