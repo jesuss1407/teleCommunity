7ccb54406224c2a520031bac3e5c96e85ee6aee8
Jorge Guillen Rivera <jorgegrr052002@gmail.com>
2023-10-27 01:51:53 -0500

    Buscar fragment: muestra y busca segun bd

--- app/src/main/java/com/example/telecommunity/DetalleUbicacionActivity.java
+++ app/src/main/java/com/example/telecommunity/DetalleUbicacionActivity.java
@@ -1,35 +1,69 @@
 package com.example.telecommunity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.telecommunity.entity.Evento;
+import com.example.telecommunity.entity.Publicaciondto;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class DetalleUbicacionActivity extends AppCompatActivity {
+    private FirebaseFirestore db;
+    private String publicacionId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_ubicacion);
 
-        // Recupera el objeto Evento de la actividad anterior
-        Evento evento = (Evento) getIntent().getSerializableExtra("evento");
+        db = FirebaseFirestore.getInstance();
 
-        // Accede a los atributos del objeto Evento
-        String titulo = evento.getTitulo();
-        String descripcion = evento.getDescripcion();
-        String hora = evento.getHora();
-        String creador = evento.getCreador();
+        // Recupera el ID de la publicación de la actividad anterior
+        publicacionId = getIntent().getStringExtra("publicacionId");
 
-        // Muestra la información en la interfaz de usuario
-        TextView tituloTextView = findViewById(R.id.tituloTextView);
-        TextView descripcionTextView = findViewById(R.id.descripcionTextView);
-        TextView horaTextView = findViewById(R.id.horaTextView);
-        TextView creadorTextView = findViewById(R.id.creadorTextView);
+        // Obtiene los datos de la publicación de Firebase Firestore
+        db.collection("publicaciones")
+                .document(publicacionId)
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        Publicaciondto publicacion = documentSnapshot.toObject(Publicaciondto.class);
 
-        tituloTextView.setText(titulo);
-        descripcionTextView.setText(descripcion);
-        horaTextView.setText("Hora: " + hora);
-        creadorTextView.setText("Creado por: " + creador);
+                        // Muestra la información en la interfaz de usuario
+                        TextView tituloTextView = findViewById(R.id.tituloTextView);
+                        TextView descripcionTextView = findViewById(R.id.descripcionTextView);
+                        TextView horaTextView = findViewById(R.id.horaTextView);
+                        TextView creadorTextView = findViewById(R.id.creadorTextView);
+
+                        String nombreCompleto = publicacion.getNombreUsuario() + " " + publicacion.getApellidoUsuario();
+                        long timestamp = publicacion.getHoraCreacion();
+                        Date date = new Date(timestamp);
+
+                        // Formatear la hora
+                        SimpleDateFormat sdfHora = new SimpleDateFormat("HH:mm");
+                        sdfHora.setTimeZone(TimeZone.getDefault());
+                        String hora = sdfHora.format(date);
+
+                        // Formatear la fecha
+                        SimpleDateFormat sdfFecha = new SimpleDateFormat("dd/MM/yy");
+                        sdfFecha.setTimeZone(TimeZone.getDefault());
+                        String fecha = sdfFecha.format(date);
+
+                        tituloTextView.setText(publicacion.getNombre());
+                        descripcionTextView.setText(publicacion.getContenido());
+                        horaTextView.setText("Hora: " + hora);
+                        creadorTextView.setText("Creado por: " + nombreCompleto);
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    Log.d("DetalleUbicacionActivity", "Error al obtener los datos de la publicación", e);
+                });
     }
 }
+
--- app/src/main/java/com/example/telecommunity/SearchFragment.java
+++ app/src/main/java/com/example/telecommunity/SearchFragment.java
@@ -2,37 +2,59 @@ package com.example.telecommunity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 
 import com.example.telecommunity.entity.Evento;
+import com.example.telecommunity.entity.Publicaciondto;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import androidx.appcompat.widget.SearchView;
+
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 
 public class SearchFragment extends Fragment implements OnMapReadyCallback {
     private GoogleMap map;
-    private com.google.android.material.search.SearchBar searchView;
+    private SearchView searchView;
+    private FirebaseFirestore db;
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
 
         // Inicializa y configura el campo de búsqueda
-        searchView = view.findViewById(R.id.searchView); // Cambiado a search_bar
-        searchView.setHint("Buscar");
+        searchView = view.findViewById(R.id.searchView);
+        searchView.setQueryHint("Buscar");
 
         // Inicializar el SupportMapFragment
         SupportMapFragment mapFragment = SupportMapFragment.newInstance();
@@ -44,66 +66,117 @@ public class SearchFragment extends Fragment implements OnMapReadyCallback {
         mapFragment.getMapAsync(this);
 
         // Configura un OnClickListener para mostrar el teclado cuando se hace clic en el campo de búsqueda
-        searchView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        searchView.setOnQueryTextFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
                 showKeyboard();
+            }
+        });
+
+        // Inicializa Firestore
+        db = FirebaseFirestore.getInstance();
+
+        // Inicializa el FusedLocationProviderClient
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
+
+        // Configura el listener para el texto de búsqueda
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                buscarPublicaciones(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Aquí puedes implementar la búsqueda en tiempo real si lo deseas
+                return false;
             }
         });
 
         return view;
     }
 
+    private void buscarPublicaciones(String query) {
+        db.collection("publicaciones")
+                .orderBy("nombre")
+                .startAt(query)
+                .endAt(query + "\uf8ff")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        map.clear();  // Limpiar el mapa antes de añadir los nuevos marcadores
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Publicaciondto publicacion = document.toObject(Publicaciondto.class);
+                            agregarMarcador(map, publicacion);
+                        }
+                    } else {
+                        Log.w("SearchFragment", "Error getting documents.", task.getException());
+                    }
+                });
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-
-        // Configura el tipo de mapa (puedes ajustar esto según tus preferencias)
+        // Configura el tipo de mapa
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
         // Configura la ubicación por defecto y el nivel de zoom
         LatLng defaultLocation = new LatLng(-12.072257, -77.079859);
-        float zoomLevel = 17;
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, zoomLevel));
-
-        // Crea dos eventos de ejemplo
-        Evento evento1 = new Evento("Evento 1", "Descripción del Evento 1", "10:00 AM", "Juan Pérez");
-        Evento evento2 = new Evento("Evento 2", "Descripción del Evento 2", "2:00 PM", "María García");
-
-        // Agrega marcadores para los eventos
-        Marker marker1 = agregarMarcador(map, evento1, -12.072230, -77.079859);
-        Marker marker2 = agregarMarcador(map, evento2, -12.070525, -77.079739);
-
-        // Configura un oyente para el clic en el marcador
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(@NonNull Marker marker) {
-                // Recupera la información del evento asociada al marcador
-                Evento evento = (Evento) marker.getTag();
-
-                if (evento != null) {
-                    // Abre una nueva actividad para mostrar información detallada
-                    Intent intent = new Intent(getContext(), DetalleUbicacionActivity.class);
-                    intent.putExtra("evento", evento);
-                    startActivity(intent);
-                }
-                return true;
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, 15));
+        // Verifica los permisos de ubicación
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            return;
+        }
+        // Obtén la ubicación actual
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(location -> {
+                    if (location != null) {
+                        LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                        map.addMarker(new MarkerOptions()
+                                .position(currentLocation)
+                                .title("Mi Ubicación")
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
+                    }
+                })
+                .addOnFailureListener(e -> Log.d("SearchFragment", "Error al obtener la ubicación actual", e));
+        // Cargar datos de Firestore
+        cargarPublicaciones();
+        // Configurar listener para clic en marcador
+        map.setOnMarkerClickListener(marker -> {
+            Publicaciondto publicacion = (Publicaciondto) marker.getTag();
+            if (publicacion != null) {
+                Intent intent = new Intent(getContext(), DetallePublicacionActivity.class);
+                intent.putExtra("publicacionId", publicacion.getId());
+                startActivity(intent);
             }
+            return true;
         });
     }
 
-    // Esta función agrega un marcador para un evento y devuelve el marcador creado
-    private Marker agregarMarcador(GoogleMap map, Evento evento, double latitud, double longitud) {
+    private void cargarPublicaciones() {
+        db.collection("publicaciones")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Publicaciondto publicacion = document.toObject(Publicaciondto.class);
+                            agregarMarcador(map, publicacion);
+                        }
+                    } else {
+                        Log.d("SearchFragment", "Error obteniendo publicaciones: ", task.getException());
+                    }
+                });
+    }
+
+    private Marker agregarMarcador(GoogleMap map, Publicaciondto publicacion) {
         MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(new LatLng(latitud, longitud));
-        markerOptions.title(evento.getTitulo());
-        markerOptions.snippet("Descripción: " + evento.getDescripcion() + "\nHora: " + evento.getHora() + "\nCreador: " + evento.getCreador());
-
-        // Asocia el evento con el marcador usando setTag
+        markerOptions.position(new LatLng(publicacion.getLatitud(), publicacion.getLongitud()));
+        markerOptions.title(publicacion.getNombre());
+        markerOptions.snippet("Descripción: " + publicacion.getContenido());
         Marker marker = map.addMarker(markerOptions);
-        marker.setTag(evento);
-
+        marker.setTag(publicacion);
         return marker;
     }
 
@@ -111,6 +184,4 @@ public class SearchFragment extends Fragment implements OnMapReadyCallback {
         InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.showSoftInput(searchView, InputMethodManager.SHOW_IMPLICIT);
     }
-
-
 }
