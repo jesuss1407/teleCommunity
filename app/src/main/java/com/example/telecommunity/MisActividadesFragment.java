0bbba957a8ae67502abb70c3e0be886e07b55609
mrojasl <mrojasl@pucp.edu.pe>
2023-11-03 13:57:38 -0500

    inicio listado de mis activ

--- app/src/main/java/com/example/telecommunity/MisActividadesFragment.java
+++ app/src/main/java/com/example/telecommunity/MisActividadesFragment.java
@@ -3,19 +3,48 @@ package com.example.telecommunity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.telecommunity.adapter.MisActividadesAdapter;
+import com.example.telecommunity.entity.ActividadDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MisActividadesFragment extends Fragment {
 
 
+    private RecyclerView recyclerView;
+    private MisActividadesAdapter actividadAdapter;
+    private List<ActividadDto> actividadList;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mis_actividades, container, false);
+        View view = inflater.inflate(R.layout.fragment_mis_actividades, container, false);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // Inicializar la lista de actividades
+        actividadList = new ArrayList<>();
+
+        // Agregar actividades de ejemplo
+        actividadList.add(new ActividadDto("1", 101, "Delegado Uno", "Actividad Uno", "Descripción de la actividad uno", "link_de_imagen", "activo"));
+        actividadList.add(new ActividadDto("2", 102, "Delegado Dos", "Actividad Dos", "Descripción de la actividad dos", "link_de_imagen", "activo"));
+        // ... Añadir más ítems según necesites
+
+        // Inicializar el adaptador con la lista de actividades
+        actividadAdapter = new MisActividadesAdapter(getActivity(), actividadList);
+
+        // Configurar el RecyclerView con el adaptador
+        recyclerView.setAdapter(actividadAdapter);
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/telecommunity/adapter/MisActividadesAdapter.java
@@ -0,0 +1,70 @@
+package com.example.telecommunity.adapter;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.telecommunity.R;
+import com.example.telecommunity.entity.ActividadDto;
+import java.util.List;
+
+public class MisActividadesAdapter extends RecyclerView.Adapter<MisActividadesAdapter.ViewHolder> {
+
+    private List<ActividadDto> actividadList;
+    private Context context;
+
+    // Constructor
+    public MisActividadesAdapter(Context context, List<ActividadDto> actividadList) {
+        this.context = context;
+        this.actividadList = actividadList;
+    }
+
+    // Crear nuevos views (invocado por el layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_actividad, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    // Reemplazar el contenido de un view (invocado por el layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        ActividadDto actividad = actividadList.get(position);
+        holder.tvNombreActividad.setText(actividad.getNombre());
+        holder.tvDelegado.setText(actividad.getDelegadoName());
+        holder.tvDescripcion.setText(actividad.getDescripcion());
+
+        // Aquí deberías cargar la imagen usando la librería que prefieras, como Glide o Picasso
+        // Por ejemplo, si usas Glide:
+        // Glide.with(context).load(actividad.getFotoLink()).into(holder.imagenActividad);
+
+        // Si no usas imágenes dinámicas, puedes omitir esta parte o poner una imagen por defecto.
+    }
+
+    // Devuelve el tamaño de tu dataset (invocado por el layout manager)
+    @Override
+    public int getItemCount() {
+        return actividadList.size();
+    }
+
+    // Proporcionar una referencia a las vistas para cada ítem de datos
+    // Los ítems de datos complejos pueden necesitar más de una vista por ítem, y
+    // proporcionas acceso a todas las vistas para un ítem de datos en un holder
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imagenActividad, ivChevronRight;
+        public TextView tvNombreActividad, tvDelegado, tvDescripcion;
+
+        public ViewHolder(View view) {
+            super(view);
+            imagenActividad = view.findViewById(R.id.imagenactividad);
+            ivChevronRight = view.findViewById(R.id.ivChevronRight);
+            tvNombreActividad = view.findViewById(R.id.tvnombreactividad);
+            tvDelegado = view.findViewById(R.id.tvdelegado);
+            tvDescripcion = view.findViewById(R.id.tvdelegado0); // Asegúrate de que este ID corresponde al de la descripción
+        }
+    }
+}
